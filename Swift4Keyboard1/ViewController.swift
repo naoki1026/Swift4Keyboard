fe81da8e72246032f0845e66e8a17effd81482a8
Naoki Arakawae <naoki.arakawa1026@gmail.com>
2019-02-18 13:59:35 +0900

    first commit

--- Swift4Keyboard1/ViewController.swift
+++ Swift4Keyboard1/ViewController.swift
@@ -8,13 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    
+ 
+    @IBOutlet var mailTextField: UITextField!
+    
+    
+    @IBOutlet var passwordTextField: UITextField!
+    
+    
+    @IBOutlet var resultLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+        mailTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        
     }
-
+//リターンキーがクリックされた時に呼び出されるデリゲートメソッド
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        //キーボードを閉じる
+        textField.resignFirstResponder()
+        
+        return true
+        
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        
+        mailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        
+        
+    }
+    
+    
+    @IBAction func signin(_ sender: Any) {
+        
+        //結果にメールテキストフィールドに入力された値とパスワードテキストフィールドに入力された値を足し算して表示する
+        
+        resultLabel.text = mailTextField.text! + " " + passwordTextField.text!
+        
+    }
+    
 
 }
-
